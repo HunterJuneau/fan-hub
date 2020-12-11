2e32085d04250749b9d41fe7b5e1ee56af8194b9
Hunter Juneau <huntlee17@gmail.com>
2020-12-11 10:32:58 -0600

    Added Auth

--- src/components/MyNavbar/index.js
+++ src/components/MyNavbar/index.js
@@ -1,22 +1,41 @@
 import React from 'react';
-import {
-  Navbar, Nav, NavItem,
-} from 'reactstrap';
+import { Navbar, Nav, NavItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import LogOut from '../Auth/logOut';
+import Auth from '../Auth';
 
-export default function MyNavbar() {
+export default function MyNavbar({ user }) {
+  const loadComponent = () => {
+    let component = '';
+    if (user === null) {
+      component = '';
+    } else if (user) {
+      component = <LogOut />;
+    } else {
+      component = <Auth />;
+    }
+    return component;
+  };
+
   return (
     <div>
       <Navbar color='dark' dark expand='md' className='justify-content-between'>
-        <Link className='navbar-brand' to='/'>Fan Hub</Link>
+        <Link className='navbar-brand' to='/'>
+          Fan Hub
+        </Link>
         <Nav className='mr-auto' navbar>
           <NavItem>
-            <Link className='nav-link' to='/'>All Leagues</Link>
+            <Link className='nav-link' to='/'>
+              All Leagues
+            </Link>
           </NavItem>
           <NavItem>
-            <Link className='nav-link' to='/your-leagues'>Your Leagues</Link>
+            <Link className='nav-link' to='/your-leagues'>
+              Your Leagues
+            </Link>
           </NavItem>
         </Nav>
+        {loadComponent()}
       </Navbar>
     </div>
   );
